26937d34f576ffa191762e424f3bfefffe1d0cf2
Mortlockus <wesleh.m@gmail.com>
2024-06-08 10:07:43 +0100

    Update art.c

--- app/boards/shields/nice_view/widgets/art.c
+++ app/boards/shields/nice_view/widgets/art.c
@@ -16,15 +16,16 @@
 #define LV_ATTRIBUTE_IMG_BALLOON
 #endif
 
-const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_BALLOON uint8_t balloon_map[] = {
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_BALLOON uint8_t
+    balloon_map[] = {
 #if CONFIG_NICE_VIEW_WIDGET_INVERTED
-    0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
-    0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
+        0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
+        0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
 #else
-    0x00, 0x00, 0x00, 0xff, /*Color of index 0*/
-    0xff, 0xff, 0xff, 0xff, /*Color of index 1*/
+        0x00, 0x00, 0x00, 0xff, /*Color of index 0*/
+        0xff, 0xff, 0xff, 0xff, /*Color of index 1*/
 #endif
-    // Balloon image data here
+        // Balloon image data here
 };
 
 const lv_img_dsc_t balloon = {
@@ -41,15 +42,16 @@ const lv_img_dsc_t balloon = {
 #define LV_ATTRIBUTE_IMG_MOUNTAIN
 #endif
 
-const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_MOUNTAIN uint8_t mountain_map[] = {
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_MOUNTAIN uint8_t
+    mountain_map[] = {
 #if CONFIG_NICE_VIEW_WIDGET_INVERTED
-    0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
-    0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
+        0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
+        0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
 #else
-    0x00, 0x00, 0x00, 0xff, /*Color of index 0*/
-    0xff, 0xff, 0xff, 0xff, /*Color of index 1*/
+        0x00, 0x00, 0x00, 0xff, /*Color of index 0*/
+        0xff, 0xff, 0xff, 0xff, /*Color of index 1*/
 #endif
-    // Mountain image data here
+        // Mountain image data here
 };
 
 const lv_img_dsc_t mountain = {
@@ -66,10 +68,11 @@ const lv_img_dsc_t mountain = {
 #define LV_ATTRIBUTE_IMG_KEYBOARD_1_1
 #endif
 
-const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_KEYBOARD_1_1 uint8_t keyboard_1_1_map[] = {
-    0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
-    0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
-    // Keyboard image data here
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_KEYBOARD_1_1 uint8_t
+    keyboard_1_1_map[] = {
+        0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
+        0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
+        // Keyboard image data here
 };
 
 const lv_img_dsc_t keyboard_1_1 = {
